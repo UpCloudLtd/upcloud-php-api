8ba121bbb90a1f6f5848c15135ebbff8d7ced7ba
AJ Kovalainen <ajk@ajk.fi>
2021-08-26 16:50:02 +0300

    feat(v3): minor code style fixes to ServerApiTrait.php

--- src/ServerApiTrait.php
+++ src/ServerApiTrait.php
@@ -79,7 +79,7 @@ trait ServerApiTrait
      */
     public function deleteServer(string $uuid, array $opts = null)
     {
-        $path = "server/$uuid" . (empty($opts) ? '' : '?'.http_build_query($opts));
+        $path = "server/$uuid" . (empty($opts) ? '' : '?' . http_build_query($opts));
 
         $response = $this->httpClient->delete($path);
         return $response;
@@ -92,7 +92,8 @@ trait ServerApiTrait
      * @param array{avoid_host: string, host: string, start_type: 'async'|'sync'} $opts Options for starting the server
      * @return object Array containing the details of the server
      */
-    public function startServer(string $uuid, array $opts = null) {
+    public function startServer(string $uuid, array $opts = null)
+    {
         $response = $this->httpClient->post(
             "server/$uuid/start",
             (empty($opts) ? null : ['server' => $opts])
@@ -107,7 +108,8 @@ trait ServerApiTrait
      * @param array $opts Options for stopping the server
      * @return object Array containing the details of the server
      */
-    public function stopServer(string $uuid, array $opts = null) {
+    public function stopServer(string $uuid, array $opts = null)
+    {
         $response = $this->httpClient->post(
             "server/$uuid/stop",
             (empty($opts) ? null : ['stop_server' => $opts])
@@ -122,7 +124,8 @@ trait ServerApiTrait
      * @param array{stop_type: 'soft'|'hard', timeout: number, timeout_action: 'destroy'|'ignore', host: number} $opts Options for the restart operation
      * @return object Array containing the details of the server
      */
-    public function restartServer(string $uuid, array $opts = null) {
+    public function restartServer(string $uuid, array $opts = null)
+    {
         $response = $this->httpClient->post(
             "server/$uuid/restart",
             (empty($opts) ? null : ['restart_server' => $opts])
@@ -136,7 +139,8 @@ trait ServerApiTrait
      * @param string $uuid UUID of the server
      * @return array Response containing status 204 and no content if the cancel succeeded
      */
-    public function cancelServerOperation($uuid) {
+    public function cancelServerOperation(string $uuid)
+    {
         $response = $this->httpClient->post("server/$uuid/cancel", null);
         return $response;
     }
