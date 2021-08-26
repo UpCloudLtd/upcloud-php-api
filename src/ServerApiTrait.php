0464a6b556724a20b9e04d0f958a63c24a98238d
AJ Kovalainen <ajk@ajk.fi>
2021-08-26 10:30:33 +0300

    chore(docs): add example documentation for Server API trait methods

--- src/ServerApiTrait.php
+++ src/ServerApiTrait.php
@@ -13,18 +13,32 @@ trait ServerApiTrait
      */
     protected $httpClient;
 
+    /**
+     * Get all the servers on this account.
+     * @return array Servers in an array
+     */
     public function getServers()
     {
         $response = $this->httpClient->get('server');
         return $response->servers->server;
     }
 
+    /**
+     * Get server details.
+     * @param $uuid UUID of the server
+     * @return array Server details in an array
+     */
     public function getServer(string $uuid)
     {
         $response = $this->httpClient->get("server/$uuid");
         return $response->server;
     }
 
+    /**
+     * Create a new server
+     * @param $server Arguments for the server creation
+     * @return array Details of the created server
+     */
     public function createServer($server)
     {
         $response = $this->httpClient->post("server", ['server' => $server]);
