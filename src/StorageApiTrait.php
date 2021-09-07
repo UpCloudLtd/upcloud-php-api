43c2e9cf4f6c64ba4623f861880f0f7d5ce6073f
AJ Kovalainen <ajk@ajk.fi>
2021-09-07 14:42:52 +0300

    feat(v3): add storage creation & deletion methods

--- src/StorageApiTrait.php
+++ src/StorageApiTrait.php
@@ -33,6 +33,31 @@ trait StorageApiTrait
     public function getStoragesByType(string $type)
     {
         $response = $this->httpClient->get("storage/$type");
+        return $response->storages->storage;
+    }
+
+    /**
+     * Create a storage.
+     *
+     * @param $storage The storage object to create
+     * @return object created storage
+     */
+    public function createStorage($storage)
+    {
+        $response = $this->httpClient->post("storage", ['storage' => $storage]);
         return $response->storage;
+    }
+
+    /**
+     * Delete a storage and optionally also its backups.
+     *
+     * @param string $uuid UUID of the storage to delete
+     * @param array{backups?: 'keep'|'keep_latest'|'delete'} $opts Optional settings to backups along with the storage
+     * @return mixed HTTP response object status 204 with no content
+     */
+    public function deleteStorage(string $storageUuid, $opts = null)
+    {
+        $path = "storags/$storageUuid" . (empty($opts) ? '' : '?' . http_build_query($opts));
+        return $this->httpClient->delete($path);
     }
 }
