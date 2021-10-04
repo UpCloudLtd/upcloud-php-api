e74c5517060b6ba75d0d94366f57fbe85f216541
AJ Kovalainen <ajk@ajk.fi>
2021-10-04 16:35:33 +0300

    feat(v3): update server creation example to use SSH keys

--- examples/createServer.php
+++ examples/createServer.php
@@ -16,6 +16,9 @@ $templates = $client->getPublicTemplates();
 $ubuntuKey = array_search('Ubuntu Server 20.04 LTS (Focal Fossa)', array_column($templates, 'title'));
 $ubuntu = $templates[$ubuntuKey];
 
+// SSH key for accessing the server
+$sshKey = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKe4zpMKPcoOBoakYdDpijz0VtgYD1f/5JhyMyzodhnr example-key';
+
 // Create the server
 $server = [
   'plan' => '1xCPU-1GB',
@@ -26,10 +29,17 @@ $server = [
   'storage_devices' => [
     'storage_device' => [
       'action' => 'clone',
+      'address' => 'virtio',
+      'size' => 10,
       'storage' => $ubuntu->uuid,
-      'size' => 25,
       'tier' => 'maxiops',
-      'title' => 'root'
+      'title' => 'root',
+    ]
+  ],
+  'login_user' => [
+    'create_password' => 'no',
+    'ssh_keys' => [
+      'ssh_key' => [$sshKey]
     ]
   ]
 ];
